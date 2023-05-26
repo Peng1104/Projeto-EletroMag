96a6b8db2939015abeb2d5ad5439434dba08bf73
Vacbo <peportella11@gmail.com>
2023-05-26 20:35:33 -0300

    Projeto v1 done

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@ int pinTensao = A0;
 
 long numero_de_medicoes = 40000;
 double conversaoLeituraArduino = 5.0 / 1023.0;
-double conversaoCorrente = 0.2 / 4.86;
-double conversaoTensao = 20 / 4.84;
+double conversaoCorrente = 0.2 / 4.91;
+double conversaoTensao = 20 / 4.95;
+int razaoTransformador = 2;
 
 void setup()
 {
@@ -20,78 +21,62 @@ void setup()
 
 void loop()
 {
-  double correnteMedia = 0;
-  double picoCorrente = 0;
-  double menorCorrente = 1000;
+  double tensaoMedida = 0;
+  double correnteMedida = 0;
+
   double tensaoMedia = 0;
+  double correnteMedia = 0;
+
   double picoTensao = 0;
-  double menorTensao = 1000;
+  double picoCorrente = 0;
+
   double P = 0;
+  double S = 0;
 
-  for (long l = 0; l < numero_de_medicoes; l++) // le 60 amostras
+  for (long l = 0; l < numero_de_medicoes; l++)
   {
-    double correnteMedida = analogRead(pinCorrente) * conversaoLeituraArduino;
-    double tensaoMedida = analogRead(pinTensao) * conversaoLeituraArduino;
+    tensaoMedida = analogRead(pinTensao) * conversaoLeituraArduino;
+    correnteMedida = analogRead(pinCorrente) * conversaoLeituraArduino;
 
-    if (correnteMedida > picoCorrente)
-    {
-      picoCorrente = correnteMedida;
-    }
     if (tensaoMedida > picoTensao)
     {
       picoTensao = tensaoMedida;
     }
-    if (correnteMedida < menorCorrente)
+    if (correnteMedida > picoCorrente)
     {
-      menorCorrente = correnteMedida;
-    }
-    if (tensaoMedida < menorTensao)
-    {
-      menorTensao = tensaoMedida;
+      picoCorrente = correnteMedida;
     }
 
-    P += correnteMedida * tensaoMedida;
-
-    correnteMedia += correnteMedida;
     tensaoMedia += tensaoMedida;
+    correnteMedia += correnteMedida;
+
+    P += tensaoMedida * correnteMedida;
   }
-  /*
-  for (long l = 0; l < numero_de_medicoes; l++)
-  {
-    P += correnteMedia * tensaoMedia;
-  } */
-  P /= numero_de_medicoes;
-  correnteMedia /= numero_de_medicoes;
+
   tensaoMedia /= numero_de_medicoes;
+  correnteMedia /= numero_de_medicoes;
 
-  //picoTensao -= tensaoMedia + menorTensao;
-  //picoCorrente -= correnteMedia + menorCorrente;
+  picoTensao -= tensaoMedia;
+  picoCorrente -= correnteMedia;
 
-  double S = picoCorrente * picoTensao / 2;
+  picoTensao *= conversaoTensao * razaoTransformador;
+  picoCorrente *= conversaoCorrente * razaoTransformador;
+
+  P /= numero_de_medicoes;
+  P *= conversaoTensao * conversaoCorrente;
+
+  S = picoCorrente * picoTensao / 2;
 
   Serial.flush();
-  Serial.print("Potencia: ");
+  Serial.print("Potencia real média: ");
   Serial.println(P);
-  Serial.print("Potencia Aparente: ");
-  Serial.println(S);
   Serial.print("Fator de Potencia: ");
   Serial.println(P / S);
-  Serial.print("Tensão média medida no arduino: ");
+  Serial.print("Offset do Amp op da tensao: ");
   Serial.println(tensaoMedia);
-  Serial.print("Corrente média medida no arduino: ");
+  Serial.print("Offset do Amp op da corrente: ");
   Serial.println(correnteMedia);
-  Serial.print("Tensão média no circuito: ");
-  Serial.println(tensaoMedia * conversaoTensao);
-  Serial.print("Corrente média no circuito: ");
-  Serial.println(correnteMedia * conversaoCorrente);
-  Serial.println("Pico de tensao");
-  Serial.println(picoTensao);
-  Serial.println("Pico de corrente");
-  Serial.println(picoCorrente);
-  Serial.println("Menor tensao");
-  Serial.println(menorTensao);
-  Serial.println("Menor corrente");
-  Serial.println(menorCorrente);
+  Serial.print("\n");
 
   delay(1000);
 }
